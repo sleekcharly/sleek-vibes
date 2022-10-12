8a8f7025e383ff75903416b5e32522811afc188a
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-12 18:14:34 +0100

    fixed bug with signout function

--- components/Center.tsx
+++ components/Center.tsx
@@ -42,12 +42,16 @@ function Center({}: Props) {
       .catch((err) => console.log('Something went wrong!', err));
   }, [spotifyApi, playlistID]);
 
+  const signOutAccount = () => {
+    signOut();
+  };
+
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
         <div
           className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white"
-          onClick={signOut}
+          onClick={() => signOutAccount()}
         >
           <div className="relative w-10 h-10">
             <Image
